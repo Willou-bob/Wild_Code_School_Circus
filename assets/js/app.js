c8f3228a948e452bdbaa2e6137f991691e186a94
Florentin Wilfried <florentinwilfried@gmail.com>
2020-01-30 09:21:43 +0100

    creation de la navbar bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,11 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+import('../css/app.scss');
 // any CSS you import will output into a single css file (app.scss in this case)
 require('../css/app.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+require('bootstrap');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
